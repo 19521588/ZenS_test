653cc2a7e2df04fc8463d8ed06be9e50fbbcac6e
truong_hung <19521588@gm.uit.edu.vn>
2023-03-10 12:11:04 +0700

    initial application

---
+++ mobile_flutter/lib/joke_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class JokeScreen extends StatefulWidget {
+  const JokeScreen({super.key});
+
+  @override
+  State<JokeScreen> createState() => _JokeScreenState();
+}
+
+class _JokeScreenState extends State<JokeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
